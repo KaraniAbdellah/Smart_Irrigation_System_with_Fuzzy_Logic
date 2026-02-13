fb24b50b82073d1429fd6eb00998b8a11f66484e
KaraniAbdellah <abdellahkarani@gmail.com>
2026-02-13 11:57:03 +0100

    feat: add file for GetData

---
+++ serverSide/getData.py
@@ -0,0 +1,28 @@
+# Get Data From API
+'''
+    🌡️ temperature (now, hourly, daily)
+    💧 humidity
+    🌧️ rain / snow
+    ☁️ clouds
+    🌅 sunrise & sunset
+    🌤️ weather condition (via code)
+    📅 forecast for 10 days
+'''
+lan = 10
+lon = 10
+
+import requests
+def getWeather(lan, log):
+    url = "https://api.open-meteo.com/v1/forecast"
+    
+    params = {
+        "latitude": lan,
+        "longitude": log,
+        "current": "temperature_2m,wind_speed_10m",
+        "hourly": "temperature_2m,relative_humidity_2m,wind_speed_10m"
+    }
+
+    response = requests.get(url, params=params)
+    return response.json()
+
+
