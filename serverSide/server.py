c3b8656e0b0ed3fb42307ef61a20e45b9d4262b9
KaraniAbdellah <abdellahkarani@gmail.com>
2026-02-14 23:00:31 +0100

    feat: init flask server

--- serverSide/server.py
+++ serverSide/server.py
@@ -1,24 +1,12 @@
 from flask import Flask
-import requests
+
+weather_app = Flask(__name__)
+@weather_app.route("/")
+def getWeatherInfo():
+    return "Weather Information ddd"
 
 
-def getWeather(lan, log):
-    url = "https://api.open-meteo.com/v1/forecast"
-    
-    params = {
-        "latitude": lan,
-        "longitude": log,
-        "current": "temperature_2m,wind_speed_10m",
-        "hourly": "temperature_2m,relative_humidity_2m,wind_speed_10m"
-    }
-
-    response = requests.get(url, params=params)
-    return response.json()
-
-
-app = Flask(__name__)
-@app.router("/getData")
-def getData(lan, log):
-    getWeather(lan=lan, log=log)
+if __name__ == "__main__":
+    weather_app.run(debug=True)
 
 
